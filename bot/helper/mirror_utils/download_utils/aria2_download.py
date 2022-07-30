8fb684c787517204d3c77263fa795a58d41064d8
akriti04s <107101789+akriti04s@users.noreply.github.com>
2022-07-30 15:39:34 +0530

    Update aria2_download.py

--- bot/helper/mirror_utils/download_utils/aria2_download.py
+++ bot/helper/mirror_utils/download_utils/aria2_download.py
@@ -34,9 +34,9 @@ def __onDownloadStarted(api, gid):
                 if sname is not None:
                     smsg, button = GoogleDriveHelper().drive_list(sname, True)
                     if smsg:
-                        dl.getListener().onDownloadError('➦ Already available !\n\n')
+                        dl.getListener().onDownloadError('Already available !\n\n')
                         api.remove([download], force=True, files=True)
-                        return sendMarkup("➦ Drive you go:", dl.getListener().bot, dl.getListener().message, button)
+                        return sendMarkup("Drive you go:", dl.getListener().bot, dl.getListener().message, button)
             if any([ZIP_UNZIP_LIMIT, LEECH_LIMIT, TORRENT_DIRECT_LIMIT, STORAGE_THRESHOLD]):
                 sleep(1)
                 limit = None
